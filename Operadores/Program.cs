72c304854d50923884b5ba0b068b6dd6127e1746
Ariomar Júnior <ariomar_junior@hotmail.com>
2021-12-01 19:53:55 -0300

    Operadores Aritméticos

---
+++ Operadores/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace operadores
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+           /* int i = 3;
+            Console.WriteLine(i);   // saída: 3
+            Console.WriteLine(i++); // saída: 3
+            Console.WriteLine(i);   // saída: 4
+
+            double a = 1.5;
+            Console.WriteLine(a);   // saída: 1.5
+            Console.WriteLine(++a); // saída: 2.5
+            Console.WriteLine(a);   // saída: 2.5
+
+            int i = 3;
+            Console.WriteLine(i);   // saída: 3
+            Console.WriteLine(i--); // saída: 3
+            Console.WriteLine(i);   // saída: 2
+
+            double a = 1.5;
+            Console.WriteLine(a);   // saída: 1.5
+            Console.WriteLine(--a); // saída: 0.5
+            Console.WriteLine(a);   // saída: 0.5
+
+            Console.WriteLine(+4);     // saída: 4
+            Console.WriteLine(-4);     // saída: -4
+            Console.WriteLine(-(-4));  // saída: 4
+
+            Console.WriteLine(13 / 5);    // saída: 2
+            Console.WriteLine(-13 / 5);   // saída: -2
+            Console.WriteLine(13 / -5);   // saída: -2
+            Console.WriteLine(-13 / -5);  // saída: 2
+
+            Console.WriteLine(13 / 5.0);       // saída: 2.6
+            int a = 13;
+            int b = 5;
+            Console.WriteLine((double)a / b);  // saída: 2.6
+
+            Console.WriteLine(5 % 4);   // saída: 1
+            Console.WriteLine(5 % -4);  // saída: 1
+            Console.WriteLine(-5 % 4);  // saída: -1
+            Console.WriteLine(-5 % -4); // saída: -1
+
+            Console.WriteLine(5 + 4);       // saída: 9
+            Console.WriteLine(5 + 4.3);     // saída: 9.3
+            */
+
+
+            Console.WriteLine(47 - 3);      // saída: 44
+            Console.WriteLine(5 - 4.3);     // saída: 0.7
+
+        }
+    }
+}
