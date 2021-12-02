d5000bada2929fb537991808b94e5b443f9b4355
Ariomar Júnior <ariomar_junior@hotmail.com>
2021-12-02 10:39:27 -0300

    FOR

---
+++ Estrutura_de_Repetição/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Estrutura_de_Repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            /*
+            Resultado da execução:
+
+            0
+            1
+            2
+            3
+            4
+            */
+        }
+    }
+}
