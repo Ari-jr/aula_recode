67d6d9418d553a40dc7d4a8824c280b0d6151319
Ariomar Júnior <ariomar_junior@hotmail.com>
2021-12-01 23:56:23 -0300

    Estruturas de Controle

---
+++ Estruturas_de_Controle/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Estruturas_de_Controle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Estrutura do IF - ELSE
+            int m = 12;
+            int n = 18;
+
+            if (m > 10)
+                if (n > 20)
+                {
+                    Console.WriteLine("Result1");
+                }
+                else
+                {
+                    Console.WriteLine("Result2");
+                }
+
+            // Estrutura do SWITCH/CASE exemplo 01
+            int caseSwitch = 1;
+
+            switch (caseSwitch)
+            {
+                case 1:
+                    Console.WriteLine("Case 1");
+                    break;
+                case 2:
+                    Console.WriteLine("Case 2");
+                    break;
+                default:
+                    Console.WriteLine("Default case");
+                    break;
+                
+                }
+
+
+            }
+    }        
+}
